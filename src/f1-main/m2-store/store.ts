28622e14e18abd6ba4bf1dd76f47fad3a7dbb4b4
Александр <80578847+PiramEVR@users.noreply.github.com>
2022-04-02 13:12:42 +0300

    profile-reducer

--- src/f1-main/m2-store/store.ts
+++ src/f1-main/m2-store/store.ts
@@ -2,14 +2,17 @@ import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import {authRed} from './reducers/authRed';
 import {SomeRed2} from './reducers/SomeRed2';
+import {profileReducer} from "./reducers/ProfileReducer";
 
-type rootReducerType = ReturnType<typeof rootReducer>
+export type rootReducerType = ReturnType<typeof rootReducer>
 const rootReducer = combineReducers({
     auth: authRed,
+    profilePage: profileReducer,
     SomeRed2,
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
+
 
 //@ts-ignore
 window.store = store

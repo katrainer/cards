242a3cf9dd29e2df22602dff24efab22f8718a43
Александр <80578847+PiramEVR@users.noreply.github.com>
2022-04-02 13:12:32 +0300

    profile-reducer

--- src/f1-main/m2-store/reducers/ProfileReducer.ts
+++ src/f1-main/m2-store/reducers/ProfileReducer.ts
@@ -1,15 +1,47 @@
 type initialStateType = typeof initialState
-const initialState = {}
 
-export const ProfileReducer = (state: initialStateType = initialState, action: GeneralActionType) => {
+
+const initialState = {
+    profile: {
+        avatar: "https://i.imgur.com/4DPdWgW.png",
+        name: 'Piram',
+    } as ProfileType
+}
+
+export const profileReducer = (state: initialStateType = initialState, action: GeneralActionType): initialStateType => {
     switch (action.type) {
+        case "SET_PROFILE":
+            return {...state, profile: action.profile}
         default:
             return {...state}
     }
 }
 
 // action creator
-type GeneralActionType = any
+type GeneralActionType = ReturnType<typeof setProfile>
+
+export const setProfile = (profile: ProfileType) => {
+    return {
+        type: "SET_PROFILE",
+        profile,
+    }as const
+}
 
 
 //thunk creator
+
+
+
+export type ProfileType = {
+    _id: string;
+    email: string;
+    name: string;
+    avatar?: string;
+    publicCardPacksCount: number;
+    created: Date;
+    updated: Date;
+    isAdmin: boolean;
+    verified: boolean;
+    rememberMe: boolean;
+    error?: string;
+};
--- src/f2-features/fe3-profile/Profile.tsx
+++ src/f2-features/fe3-profile/Profile.tsx
@@ -1,5 +1,13 @@
+import {useSelector} from "react-redux";
+import {rootReducerType} from "../../f1-main/m2-store/store";
+import s from "./Profile.module.css"
+
 export const Profile = () => {
+    const profile = useSelector((state: rootReducerType) => state.profilePage.profile)
+
     return <div>
-        Profile
+        <img className={s.avatar} src={profile.avatar} alt="avatar"/>
+       <div> {profile.name}</div>
+        <button>Edit mode</button>
     </div>
 }

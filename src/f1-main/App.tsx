d6bdac044371b0e0477821e8ef836aa1bf5f1d4f
Katrain <laub94@list.ru>
2022-04-02 09:30:59 +0300

    create api.ts

--- src/f1-main/App.tsx
+++ src/f1-main/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import './App.module.css';
-import {Login} from "../f2-features/fe1-auth/a1-login/Login";
+import {Login} from '../f2-features/fe1-auth/a1-login/Login';
 import {Register} from '../f2-features/fe1-auth/a2-register/Register';
-import {Profile} from "../f2-features/fe3-profile/Profile";
+import {Profile} from '../f2-features/fe3-profile/Profile';
 import {PasswordRecovery} from '../f2-features/fe1-auth/a3-passwordRecovery/PasswordRecovery';
 import {PasswordChange} from '../f2-features/fe1-auth/a4-passwordChange/PasswordChange';
 import {Error} from '../f2-features/fe4-error/Error';
 import {Test} from '../f2-features/fe0-test/Test';
-import {routesPath} from "./m1-ui/u2-routes/routesPath";
+import {routesPath} from './m1-ui/u2-routes/routesPath';
 import {Header} from './m1-ui/u1-header/Header';
 import s from './App.module.css'
 

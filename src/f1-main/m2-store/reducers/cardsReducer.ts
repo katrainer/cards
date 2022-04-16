d9ff4d72dc180cca60e3c6a13f6ec6b1ea4ca5c4
Katrain <laub94@list.ru>
2022-04-16 18:21:36 +0300

    to start put grade

--- src/f1-main/m2-store/reducers/cardsReducer.ts
+++ src/f1-main/m2-store/reducers/cardsReducer.ts
@@ -1,13 +1,15 @@
-import {apiCards, CardsType, CreateCardsType, GetCardsResponseType} from "../../m3-API/apiCards";
-import {AppThunk} from "../store";
-import axios from "axios";
+import {apiCards, CardsType, CreateCardsType, GetCardsResponseType, GradeType} from '../../m3-API/apiCards';
+import {AppThunk} from '../store';
+import axios from 'axios';
 
 enum EnumCardsReducerActionType {
     setCards = 'CARDS/SET-CARDS',
-    updateGrade = 'CARDS/UPDATE-GRADE',
+    updateGrade = 'CARDS/UPDATE-GRADE', // need remove?
     // removeCard = 'CARDS/REMOVE-CARD',
     createCards = 'CARDS/CREATE-CARD',
     updateCard = 'CARDS/UPDATE-CARD',
+    updateGradeCard = 'CARDS/UPDATE-GRADE-CARD', //new method
+    changeRequestStatusType = 'CARDS/CHANGE-REQUEST-STATUS-TYPE',
 }
 
 const initialState = {
@@ -19,7 +21,8 @@ const initialState = {
     pageCount: 50,
     packUserId: '',
     sortCards: '0updated',
-    cardsPack_id: '6255617c2a4150000412ed70'
+    cardsPack_id: '6255617c2a4150000412ed70',
+    requestStatus: 'idle' as RequestStatusType,
 }
 
 export const cardsReducer = (state: initialStateType = initialState, action: CardsReducerActionType): initialStateType => {
@@ -39,11 +42,20 @@ export const cardsReducer = (state: initialStateType = initialState, action: Car
                     }
                 })]
             }
+        case EnumCardsReducerActionType.changeRequestStatusType:
+            return {...state, ...action.payload}
         default:
             return {...state}
     }
 }
 
+//action
+const changeRequestStatusAC = (requestStatus: RequestStatusType) => {
+    return {
+        type: EnumCardsReducerActionType.changeRequestStatusType,
+        payload: {requestStatus}
+    } as const
+}
 const setCardsAC = (cardsData: GetCardsResponseType) => {
     return {
         type: EnumCardsReducerActionType.setCards,
@@ -57,13 +69,25 @@ export const updateGradeAC = (grade: number, id: string) => {
         payload: {grade, id},
     } as const
 }
+const updateGradeCardAC = (grade: GradeType | number, card_id: string) => { //new method
+    return {
+        type: EnumCardsReducerActionType.updateGradeCard,
+        payload: {
+            grade,
+            card_id
+        },
+    } as const
+}
 
+
+//thunk
 export const setCardsTC = (): AppThunk => async (dispatch, getState) => {
-
-    const {cardsPack_id, sortCards, page, pageCount } = getState().cards;
+    dispatch(changeRequestStatusAC('loading'))
+    const {cardsPack_id, sortCards, page, pageCount} = getState().cards;
     try {
         const res = await apiCards.getCards({cardsPack_id, sortCards, page, pageCount})
         dispatch(setCardsAC(res.data))
+        dispatch(changeRequestStatusAC('succeeded'))
     } catch (e) {
         if (axios.isAxiosError(e) && e.response) {
             const errorMessage = e.response.data.error;
@@ -72,11 +96,12 @@ export const setCardsTC = (): AppThunk => async (dispatch, getState) => {
     }
 }
 
-export const updateCardTC = (_id: string, question?: string, answer?:string):AppThunk=> async (dispatch,getState)=>{
-
-    try{
+export const updateCardTC = (_id: string, question?: string, answer?: string): AppThunk => async (dispatch, getState) => {
+    dispatch(changeRequestStatusAC('loading'))
+    try {
         await apiCards.updateCards({_id, question, answer})
         dispatch(setCardsTC())
+        dispatch(changeRequestStatusAC('succeeded'))
     } catch (e) {
         if (axios.isAxiosError(e) && e.response) {
             const errorMessage = e.response.data.error;
@@ -85,11 +110,12 @@ export const updateCardTC = (_id: string, question?: string, answer?:string):App
     }
 }
 
-export const createCardTC = (data: CreateCardsType):AppThunk=> async (dispatch,getState)=>{
-
-    try{
+export const createCardTC = (data: CreateCardsType): AppThunk => async (dispatch, getState) => {
+    dispatch(changeRequestStatusAC('loading'))
+    try {
         await apiCards.createCard(data)
         dispatch(setCardsTC())
+        dispatch(changeRequestStatusAC('succeeded'))
     } catch (e) {
         if (axios.isAxiosError(e) && e.response) {
             const errorMessage = e.response.data.error;
@@ -98,10 +124,12 @@ export const createCardTC = (data: CreateCardsType):AppThunk=> async (dispatch,g
     }
 }
 
-export const removeCardTC = (id: string):AppThunk=> async (dispatch)=>{
-    try{
+export const removeCardTC = (id: string): AppThunk => async (dispatch) => {
+    dispatch(changeRequestStatusAC('loading'))
+    try {
         await apiCards.removeCard(id)
         dispatch(setCardsTC())
+        dispatch(changeRequestStatusAC('succeeded'))
     } catch (e) {
         if (axios.isAxiosError(e) && e.response) {
             const errorMessage = e.response.data.error;
@@ -109,8 +137,21 @@ export const removeCardTC = (id: string):AppThunk=> async (dispatch)=>{
         }
     }
 }
+export const updateGradeCard = (grade: GradeType, card_id: string): AppThunk => async dispatch => {
+    dispatch(changeRequestStatusAC('loading'))
+    try {
+        dispatch(changeRequestStatusAC('succeeded'))
+    } catch (e) {
+
+    }
+}
+
+
 //type
 type initialStateType = typeof initialState
 export type CardsReducerActionType =
     | ReturnType<typeof setCardsAC>
     | ReturnType<typeof updateGradeAC>
+    | ReturnType<typeof updateGradeCardAC>
+    | ReturnType<typeof changeRequestStatusAC>
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
